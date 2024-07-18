a42b6d81aa5f550f2d8b22049e541d990f5fc85f
Petar Petrov <ppetrov@vexelon.net>
2024-07-19 00:19:56 +0200

    Finish first impl of README gen

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-## Hi there 👋
+### Pinned
 
-<!--
-**petarov/petarov** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+**[game-off-2017](https://github.com/kenamick/game-off-2017)** - **114x**:star2: **25x**:eyes: | <sup>JavaScript</sup> | <sub>👊  8-bit Retro Beat 'em up with Game Boy graphics </sub>
 
-Here are some ideas to get you started:
+**[google-android-app-ids](https://github.com/petarov/google-android-app-ids)** - **68x**:star2: **14x**:eyes: | <sup>Python</sup> | <sub>Google Android apps found on the Play Store</sub>
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+**[awesome-mdm-dev](https://github.com/petarov/awesome-mdm-dev)** - **67x**:star2: **8x**:eyes: | <sub>:iphone: A curated list of Mobile Device Management (MDM) developer resources</sub>
+
+**[savagewheels](https://github.com/kenamick/savagewheels)** - **33x**:star2: **17x**:eyes: | <sup>C++</sup> | <sub>:checkered_flag: 2D car crashing game armageddon</sub>
+
+**[shut-up-bird](https://github.com/petarov/shut-up-bird)** - **21x**:star2: **4x**:eyes: | <sup>Python</sup> | <sub>:bird: Put your tweets/likes in an EPUB and delete them like a boss</sub>
+
+**[translitbg.js](https://github.com/petarov/translitbg.js)** - **20x**:star2: **7x**:eyes: | <sup>JavaScript</sup> | <sub>Българска транслитерация (Bulgarian to Latin transliteration for JS)</sub>
+
+**[strider](https://github.com/kenamick/strider)** - **12x**:star2: **7x**:eyes: | <sup>JavaScript</sup> | <sub>:bomb: Strider is a 2D sci-fi platformer (Game Off 2015 entry)</sub>
+
+**[appstore-web-search](https://github.com/petarov/appstore-web-search)** - **7x**:star2: **0x**:eyes: | <sup>JavaScript</sup> | <sub>Search the Apple App Store in your browser</sub>
+
---
+++ gen.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+var (
+	MIN_STARS = 7
+	USERNAME  = "petarov"
+	ORGS      = [...]string{"kenamick", "vexelon-dot-net"}
+)
+
+func getRepositories(token string) (result []*github.Repository, err error) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	result = make([]*github.Repository, 0)
+
+	repos, _, err := client.Repositories.List(ctx, USERNAME, &github.RepositoryListOptions{
+		Visibility: "public",
+		// Affiliation: "owner,organization_member",
+		// Type:        "owner,public",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error fetching user repositories: %v", err)
+	}
+
+	for _, repo := range repos {
+		if *repo.StargazersCount >= MIN_STARS && !*repo.Fork {
+			result = append(result, repo)
+		}
+	}
+
+	// -----
+	opt := &github.RepositoryListByOrgOptions{
+		ListOptions: github.ListOptions{PerPage: 20},
+	}
+
+	for _, org := range ORGS {
+		opt.Page = 0
+
+		for {
+			repos, resp, err := client.Repositories.ListByOrg(ctx, org, opt)
+			if err != nil {
+				return nil, fmt.Errorf("error fetching orga repositories: %v", err)
+			}
+
+			for _, repo := range repos {
+				if *repo.StargazersCount >= MIN_STARS && !*repo.Fork {
+					result = append(result, repo)
+				}
+			}
+
+			if resp.NextPage == 0 {
+				break
+			}
+
+			opt.Page = resp.NextPage
+		}
+	}
+
+	return result, nil
+}
+
+func writeReadme(repos []*github.Repository) error {
+	out, err := os.Create("README.md")
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	out.WriteString("### Pinned\n\n")
+
+	for _, repo := range repos {
+		forks := fmt.Sprintf("**%dx**:eyes:", repo.GetForksCount())
+		lang := ""
+		if len(repo.GetLanguage()) > 0 {
+			lang = fmt.Sprintf("<sup>%s</sup> | ", repo.GetLanguage())
+		}
+
+		out.WriteString(fmt.Sprintf("**[%s](%s)** - **%dx**:star2: %s | %s<sub>%s</sub>\n\n", repo.GetName(), repo.GetHTMLURL(), repo.GetStargazersCount(), forks, lang, *repo.Description))
+	}
+
+	return nil
+}
+
+func main() {
+	token := os.Getenv("GITHUB_TOKEN")
+	repos, err := getRepositories(token)
+	if err != nil {
+		log.Fatalf("error fetching repositories: %v", err)
+	}
+
+	sort.Slice(repos, func(i, j int) bool { return *repos[i].StargazersCount > *repos[j].StargazersCount })
+
+	for _, repo := range repos {
+		if *repo.StargazersCount >= MIN_STARS {
+			fmt.Printf("Repo: %s - %s\nStars: %d  Forks: %d  Lang: %s\n\n", *repo.Name, *repo.Description, *repo.StargazersCount, *repo.ForksCount,
+				repo.GetLanguage())
+		}
+	}
+
+	writeReadme(repos)
+	if err != nil {
+		log.Fatalf("error writing README: %v", err)
+	}
+}
