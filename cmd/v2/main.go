e7da10f68cdadb61d638d7c1ea599cb515aae913
Petar Petrov <ppetrov@vexelon.net>
2025-01-28 00:17:23 +0100

    Simplify footer

--- cmd/v2/main.go
+++ cmd/v2/main.go
@@ -346,8 +346,8 @@ func writeReadme(repos []Entry, pulls []Entry, issues []Entry, comments []Entry)
 	}
 
 	out.WriteString("\n<sub>updated: ")
-	out.WriteString(time.Now().UTC().Format("2006-01-02 15:04"))
-	out.WriteString(" (UTC) | :envelope: gh(@]vexelon.net</sub>")
+	out.WriteString(time.Now().UTC().Format("2006-01-02"))
+	out.WriteString(" | gh(@]vexelon.net</sub>")
 
 	return nil
 }
